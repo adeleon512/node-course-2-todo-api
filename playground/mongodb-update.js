07dcb40e1b61a3469f723137b5d18bf8bf9337f6
Adam <adam.deleon512@gmail.com>
2017-05-15 21:15:54 -0500

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,50 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("591a4f081f621ea890075563")
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').findOneAndUpdate({
+    //     name: "Jen"
+    // }, {
+    //     $set: {
+    //         name: "Adam"
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("591a5cdc1f621ea8900758e0")
+    }, {
+        $set: {
+            name: "Adam"
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+    // db.close();
+});
+
