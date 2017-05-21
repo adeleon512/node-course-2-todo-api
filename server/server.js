73830d76556a5c45c9ebf536618344ec8113f2db
Adam <adam.deleon512@gmail.com>
2017-05-21 11:21:37 -0500

    Set up app for heroku

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var {User} = require('./models/user');
 // mongod.exe --dbpath /Users/Adam/mongo-data
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,8 +50,8 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started up at prt ${port}.`);
 });
 
 module.exports = {app};
